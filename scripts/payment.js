6bf5f98f2c0f9097935b32ba8fa2a41d2efa3d07
Gibzclt <gibinninan5@gmail.com>
2021-08-10 11:17:09 +0530

    fixed reload bug in payment.html

--- scripts/payment.js
+++ scripts/payment.js
@@ -4,13 +4,20 @@ paynowBtn.onclick = () => {
   location.href = "payment.html";
 }
 
+const paynowBtnToggle = () => {
+  if(localStorage.getItem("Login") === "true"){
+    paynowBtn.disabled = false;
+  } else{
+    paynowBtn.disabled = true;
+  } 
+}
+
+paynowBtnToggle();
 loginBtn.addEventListener("click", () =>{
-  location.href = "payment.html";
-  location.reload();
+  setTimeout(paynowBtnToggle, 1000);
 })
 
-if(localStorage.getItem("Login") === "true"){
-  paynowBtn.disabled = false;
-} else{
-  paynowBtn.disabled = true;
-}
+headerLogin.addEventListener("click" , ()=> {setTimeout(paynowBtnToggle, 1000)});
+
+
+
